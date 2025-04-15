0aaad7cbcf59858b2f096c2dbc22c7231c945362
Hak0bian <hakobyan7171@gmail.com>
2025-04-15 13:49:52 +0400

    Skeleton

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,14 +1,55 @@
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getUsersThunk } from "../../store/reducers/getUsersReducer";
 import UserCard from "../UserCard/UserCard";
 import Pagination from "../Pagination/Pagination";
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import st from "./Users.module.css"
 
 const Users = () => {
-    const { users, totalUsersCount, totalPagesCount } = useSelector((state) => state.users);
+    const dispatch = useDispatch();
+    const { users, page, totalUsersCount, totalPagesCount } = useSelector((state) => state.users);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (users.length > 0) {
+            setLoading(false); 
+        }
+    }, [users]);
+
+    useEffect(() => {
+        dispatch(getUsersThunk(page)); 
+    }, [page]);
 
     return (
         <section>
-            <UserCard users={users} />
-            <Pagination totalUsersCount={totalUsersCount} totalPagesCount={totalPagesCount} />
+            {loading ? (
+                <div className={st.usersContainer}>
+                    {[...Array(20)].map((_, ind) => (
+                        <div key={ind} className={st.userDivSkeleton}>
+                            <Skeleton  className={st.imgSkeleton} />
+                            <Skeleton  className={st.titleSkeleton} />
+                            <Skeleton  className={st.btnSkeleton} />
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <UserCard users={users} />
+            )}
+
+
+            {loading ? (
+                <div className={st.buttonsDivSkeleton}>
+                    {[...Array(7)].map((_, ind) => (
+                        <button key={ind}>
+                            <Skeleton className={st.pageBtnSkeleton} />
+                        </button>
+                    ))}
+                </div>
+            ) : (
+                <Pagination totalUsersCount={totalUsersCount} totalPagesCount={totalPagesCount} />
+            )}
         </section>
     );
 };
