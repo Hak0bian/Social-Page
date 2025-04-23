1221f4ecac79278f742146525f2631fddf0f3b98
Hak0bian <hakobyan7171@gmail.com>
2025-04-23 19:09:33 +0400

    image size

--- src/components/ProfileButtons/ProfileButtons.jsx
+++ src/components/ProfileButtons/ProfileButtons.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { changePhotoThunk, logOutThunk } from '../../store/reducers'
 import { LuUserPen } from "react-icons/lu";
 import { MdOutlineAddAPhoto } from "react-icons/md";
@@ -7,6 +7,7 @@ import { TbLogout } from "react-icons/tb";
 import st from "./ProfileButtons.module.css"
 
 const ProfileButtons = ({setEditProfile}) => {
+    const {fileError} = useSelector((state) => state.photo)
     const dispatch = useDispatch()
 
     const handleChangePhoto = (e) => {
@@ -20,17 +21,20 @@ const ProfileButtons = ({setEditProfile}) => {
     }
 
     return (
-        <div className={st.buttonsDiv}>
-            <button onClick={() => setEditProfile(true)} className={st.editBtn}>
-                <LuUserPen/>
-            </button>
-            <label className={st.uploadBtn}>
-                <MdOutlineAddAPhoto/>
-                <input type="file" onChange={handleChangePhoto} hidden/>
-            </label>
-            <button onClick={handleLogOut} className={st.logOutBtn}>
-                <TbLogout/>
-            </button>
+        <div>
+            <div className={st.buttonsDiv}>
+                <button onClick={() => setEditProfile(true)} className={st.editBtn}>
+                    <LuUserPen/>
+                </button>
+                <label className={st.uploadBtn}>
+                    <MdOutlineAddAPhoto/>
+                    <input type="file" onChange={handleChangePhoto} hidden/>
+                </label>
+                <button onClick={handleLogOut} className={st.logOutBtn}>
+                    <TbLogout/>
+                </button>
+            </div>
+            { fileError && <p className={st.fileError}>{fileError}</p> }
         </div>
     )
 }
--- src/store/reducers/changePhoto.js
+++ src/store/reducers/changePhoto.js
@@ -1,8 +1,10 @@
 import { API } from "../../api/api"
 
 const CHANGE_PHOTO = "CHANGE_PHOTO"
+const FILE_ERROR = "FILE_ERROR"
 const initState = {
-    file: {}
+    file: {},
+    fileError: ""
 }
 
 export const changePhotoReducer = (state = initState, action) => {
@@ -10,7 +12,13 @@ export const changePhotoReducer = (state = initState, action) => {
         case CHANGE_PHOTO :
             return {
                 ...state,
-                file: action.payload
+                file: action.payload,
+                fileError: ""
+            }
+        case FILE_ERROR : 
+            return {
+                ...state,
+                fileError: action.payload
             }
         default :
             return state
@@ -18,10 +26,18 @@ export const changePhotoReducer = (state = initState, action) => {
 }
 
 const changePhotoAC = (file) => ({type: CHANGE_PHOTO, payload : file})
+const fileErrorAC = (err) => ({type: FILE_ERROR, payload: err})
+
 export const changePhotoThunk = (file) => {
     return(dispatch) => {
         API.changePhoto(file)
-        .then((res) => dispatch(changePhotoAC(res.data)))
+        .then((res) => {
+            if(res?.data?.resultCode === 0){
+                dispatch(changePhotoAC(res?.data))
+            }else if(res?.data?.resultCode === 1){
+                dispatch(fileErrorAC(res?.data?.messages[0]))
+            }
+        })
         .catch((err) => console.log(err))
     }
 }
